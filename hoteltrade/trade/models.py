5cc623cdc4a13e2594671ed1cb162e2f9e07bcab
Dmitry <dms72@list.ru>
2022-02-09 08:52:59 +0700

    Таблица документов и чеков

--- hoteltrade/trade/models.py
+++ hoteltrade/trade/models.py
@@ -57,3 +57,69 @@ class Good(models.Model):
         verbose_name='Товар'
         verbose_name_plural='Товары'
         ordering=['title',]
+
+    # Тип документа
+
+
+class TypeDoc(models.Model):
+    title = models.CharField(max_length=70, verbose_name='Тип документа')
+
+    def __str__(self):
+        return self.title
+
+
+# Журнал документов
+
+class Doc(models.Model):
+    nomerdoc=models.CharField(max_length=20,verbose_name="Номер документв")
+    datadoc=models.DateField(verbose_name='Дата документа')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Создан')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Обновлен')
+    dealer=models.ForeignKey(Saler,verbose_name='Поставщик',on_delete=models.PROTECT,null=True)
+    buytotal=models.DecimalField(max_digits=8,decimal_places=2,blank=True,null=True,default=0.00)
+    saletotal=models.DecimalField(max_digits=8,decimal_places=2,blank=True,null=True,default=0.00)
+    transaction=models.BooleanField(verbose_name='Проведен',default=False)
+    typedoc=models.ForeignKey(TypeDoc,verbose_name='Тип документа',on_delete=models.PROTECT)
+    def __str__(self):
+        return str(self.nomerdoc)
+    class Meta:
+        verbose_name='Документ'
+        verbose_name_plural='Документы'
+
+
+
+# Журнал табличной части документов
+class DocJurnal(models.Model):
+    iddoc=models.ForeignKey(Doc,verbose_name='Документ',on_delete=models.PROTECT)
+    title=models.ForeignKey(Good,verbose_name='Наименование товара',on_delete=models.PROTECT)
+    volume=models.DecimalField(max_digits=8,decimal_places=2,blank=True)
+    buyprice=models.DecimalField(max_digits=8,decimal_places=2,blank=True)
+    percent=models.DecimalField(max_digits=8,decimal_places=2,blank=True)
+    saleprice=models.DecimalField(max_digits=8,decimal_places=2,blank=True)
+    buytotal=models.DecimalField(max_digits=8,decimal_places=2,blank=True)
+    saletotal=models.DecimalField(max_digits=8,decimal_places=2,blank=True)
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Создан')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Обновлен')
+    typedoc=models.IntegerField(null=True)
+    nomercheck=models.IntegerField(blank=True,null=True,verbose_name='Номер чека')
+    class Meta:
+        verbose_name='Наименование'
+        verbose_name_plural='Наименования'
+        ordering=['title',]
+# Номер чека
+class Check(models.Model):
+    nomercheck=models.IntegerField()
+    def __str__(self):
+        return str(self.nomercheck)
+# Временная таблица
+class TempCheck(models.Model):
+    idgood=models.IntegerField()
+    title=models.CharField(max_length=100)
+    unit=models.CharField(max_length=50)
+    volume=models.DecimalField(max_digits=8,decimal_places=2,blank=True)
+    price=models.DecimalField(max_digits=8,decimal_places=2,blank=True)
+    total=models.DecimalField(max_digits=8,decimal_places=2,blank=True)
+    maxvolume=models.DecimalField(max_digits=8,decimal_places=2,blank=True)
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Создан')
+    def __str__(self):
+        return self.title
